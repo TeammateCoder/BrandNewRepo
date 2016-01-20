c472006ac9cc1e2edb2dfde25da8bcc87cc5b713
TeammateCoder <TeammateCoder@gmail.com>
2016-01-20 10:18:02 -0800

    Revert "Delete team again"
    This reverts commit 7c8abc8af243288c5cd48eee18106b34c6ff1a80.

---
+++ MyNewConsoleApp/MyNewConsoleApp/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewConsoleApp
+{
+    class Team
+    {
+        public List<Person> People { get; set; }
+    }
+}
